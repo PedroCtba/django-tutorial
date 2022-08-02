0aee8ba04b741bbf0aaed0212cc8f860c6499ab9
PedroCtba <miyasakigazola@gmail.com>
2022-08-02 08:31:55 -0300

    build: add conection with postgree database | add a 'Receita' model to models.py

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+from .secret import postgree_password, postgree_host
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +78,11 @@ WSGI_APPLICATION = 'alurareceita.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alura_receita',
+        'USER': 'postgres',
+        'PASSWORD': postgree_password,
+        'HOST': postgree_host,
     }
 }
 
--- receitas/views.py
+++ receitas/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    dados = {
+    'nome_das_receitas': {
+                        1: "Lasanha",
+                        2: "Sopa de Legumes",
+                        3: "Sorvete",
+                        4: "Bolo de chocolate"
+                        }
+    }
+
+    return render(request, 'index.html', dados)
 
 def receita(request):
     return render(request, 'receita.html')
