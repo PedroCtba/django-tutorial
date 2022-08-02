1bb9aae92487df9a4c7ab0a44ec8a8e51b32f853
PedroCtba <miyasakigazola@gmail.com>
2022-08-01 21:59:52 -0300

    added base.html for HTML inheritance

--- receitas/views.py
+++ receitas/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    # Return thr HTML file at this view
     return render(request, 'index.html')
+
+def receita(request):
+    return render(request, 'receita.html')
